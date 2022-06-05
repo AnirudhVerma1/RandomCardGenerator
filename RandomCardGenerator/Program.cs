9a192a8d29c0473395343256af96fa6197c4f5a8
Anirudh Verma <vermajianirudh369@outlook.com>
2022-06-05 17:10:58 +0530

    Added input constraints.

--- RandomCardGenerator/Program.cs
+++ RandomCardGenerator/Program.cs
@@ -4,17 +4,25 @@
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter the number of cards to generate(1 to 52): ");
             string? input = Console.ReadLine();
-            if(int.TryParse(input, out int value))
+            if (int.TryParse(input, out int value))
             {
-                HashSet<string> cards = CardPicker.RandomCardPicker(value);
-                int count = 1;
-                foreach(string card in cards)
+                if ((value < 53) && (value > 0))
                 {
-                    Console.WriteLine(card + " " + count.ToString());
-                    count++;
+                    HashSet<string> cards = CardPicker.RandomCardPicker(value);
+                    int count = 1;
+                    foreach (string card in cards)
+                    {
+                        Console.WriteLine("Card Number " + count.ToString() + " : " + card);
+                        count++;
+                    }
                 }
+                else
+                    Console.WriteLine("Enter the number between 1 to 52 only.");
             }
+            else
+                Console.WriteLine("Enter a valid number.");
         }
     }
 }
