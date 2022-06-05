8d3dcc3632846baf4146bb1fb1cae28d1c1d7e28
Anirudh Verma <vermajianirudh369@outlook.com>
2022-06-05 17:04:40 +0530

    Add project files.

---
+++ RandomCardGenerator/CardPicker.cs
@@ -0,0 +1,47 @@
+﻿namespace RandomCardGenerator
+{
+    internal class CardPicker
+    {
+        public static HashSet<string> RandomCardPicker(int noOfCards)
+        {
+            HashSet<string> cards = new();
+            for(int i = 0; i < noOfCards;)
+            {
+                if (cards.Add(RandomValue() + " of " + RandomCategory()))
+                    i++;
+            }
+            return cards;
+        }
+
+        static Random random = new Random();
+        private static string RandomCategory()
+        {
+            //Random random = new Random();
+            int value = random.Next(1,5);
+            if (value == 1)
+                return "Diamonds";
+            if (value == 2)
+                return "Clubs";
+            if (value == 3)
+                return "Spades";
+            if (value == 4)
+                return "Hearts";
+            return "Check the Next method maxValue";
+        }
+
+        private static string RandomValue()
+        {
+            //Random random = new Random();
+            int value = random.Next(1, 14);
+            if (value == 1)
+                return "Ace";
+            if (value == 11)
+                return "Jack";
+            if (value == 12)
+                return "Queen";
+            if (value == 13)
+                return "King";
+            return value.ToString();
+        }
+    }
+}
---
+++ RandomCardGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RandomCardGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? input = Console.ReadLine();
+            if(int.TryParse(input, out int value))
+            {
+                HashSet<string> cards = CardPicker.RandomCardPicker(value);
+                int count = 1;
+                foreach(string card in cards)
+                {
+                    Console.WriteLine(card + " " + count.ToString());
+                    count++;
+                }
+            }
+        }
+    }
+}
